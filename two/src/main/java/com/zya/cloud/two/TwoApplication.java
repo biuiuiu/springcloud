e8eef934431e99ceb86d38f3cd6fd988358efac6
yazhao <yazhao5692@fiberhome.com>
2018-09-21 11:31:29 +0800

    服务注册到consul

--- two/src/main/java/com/zya/cloud/two/TwoApplication.java
+++ two/src/main/java/com/zya/cloud/two/TwoApplication.java
@@ -2,8 +2,10 @@ package com.zya.cloud.two;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TwoApplication {
 
 	public static void main(String[] args) {
