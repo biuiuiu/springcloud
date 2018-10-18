617534db53c8a018cb8b3ce4d4c5dbcbaaf8bbf3
yazhao <yazhao5692@fiberhome.com>
2018-10-18 16:49:01 +0800

    1

--- two/src/main/java/com/zya/cloud/two/ui/test/DivideSearch.java
+++ two/src/main/java/com/zya/cloud/two/ui/test/DivideSearch.java
@@ -1,5 +1,7 @@
 package com.zya.cloud.two.ui.test;
 
+import java.util.Arrays;
+
 /**
  * 二分查找
  * 
@@ -12,6 +14,7 @@ public class DivideSearch {
 		int index = 0;
 		System.out.println(getIndex(arr, 0, arr.length - 1, 56, index));
 		System.out.println(getIndex(arr, 56));
+		System.out.println(getIndexFibonacci(arr, 56));
 	}
 
 	static int getIndex(int[] arr, int left, int right, int k, int index) {
@@ -40,7 +43,6 @@ public class DivideSearch {
 		while (left < right) {
 			// 差值查找
 			int mid = left + (k - arr[left]) * arr.length / (arr[right] - arr[left]);
-			System.out.println(mid);
 			if (arr[mid] < k) {
 				left = mid + 1;
 			} else if (arr[mid] > k) {
@@ -51,27 +53,54 @@ public class DivideSearch {
 		}
 		return -1;
 	}
-	
-	static void getIndexFibonacci(int[] arr,int k){
+
+	/**
+	 * 斐波那契查找方法
+	 * 
+	 * @param arr
+	 * @param key
+	 * @return
+	 */
+	static int getIndexFibonacci(int[] arr, int key) {
 		int[] fibonacci = getFibonacci(20);
 		int index = 0;
+		int left = 0;
+		int right = arr.length - 1;
 		for (int i = 0; i < fibonacci.length; i++) {
 			if (fibonacci[i] > arr.length) {
 				index = i;
 				break;
 			}
 		}
+		int[] newArray = Arrays.copyOf(arr, fibonacci[index]);
+		// 补齐数组到f[index]的长度
+		for (int i = right + 1; i < fibonacci[index]; i++) {
+			newArray[i] = arr[right];
+		}
+		while (left <= right) {
+			int mid = left + fibonacci[index - 1] - 1;
+			if (key < newArray[mid]) {
+				right = mid - 1;
+				index = index - 1;
+			} else if (key > newArray[mid]) {
+				left = mid + 1;
+				index = index - 2;
+			} else {
+				return mid;
+			}
+		}
+		return -1;
 	}
-	
-	static int[] getFibonacci(int maxSize){
+
+	static int[] getFibonacci(int maxSize) {
 		if (maxSize < 2) {
 			return null;
 		}
 		int[] fibonacci = new int[maxSize];
-		fibonacci[0] = 0;
+		fibonacci[0] = 1;
 		fibonacci[1] = 1;
 		for (int i = 2; i < maxSize; i++) {
-			fibonacci[i] = fibonacci[i-1]+fibonacci[i-2];
+			fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
 		}
 		return fibonacci;
 	}
